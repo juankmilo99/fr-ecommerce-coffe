b6a72b763fac8672b9d0df441dfafd902b64bffd
juan rodriguez <juan.rodriguezs@usantoto.edu.co>
2024-11-26 00:57:41 -0500

    cancel page

---
+++ app/(routes)/cancel/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CancelPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Order Cancelled</h1>
+            <p>Your order has been cancelled. If you have any questions, please contact our support team.</p>
+        </div>
+    );
+};
+
+export default CancelPage;
